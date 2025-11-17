19de4899f68637589cf70f367a1b310501298433
johnmarkfrncsc <johnmarkfrancisco40@yahoo.com>
2025-11-18 02:21:09 +0800

    feat: add middleware validation for Rent API requests
    - Implemented required field validation to check for missing fields
    - Added string trimming and empty string checks
    - Added type validation for strings and numbers
    - Combined type and value checks into clean if-else blocks per field
    - Ensured proper error responses for invalid input
    - Prepared middleware to pass cleaned values to controllers

--- backend/src/middleware/validations/ValidateRent.js
+++ backend/src/middleware/validations/ValidateRent.js
@@ -8,27 +8,53 @@ const ValidateRent = (req, res, next) => {
       "rentPrice",
       "rentImageURL",
     ];
+    const fieldTypes = {
+      rentTitle: String,
+      rentDescription: String,
+      rentCategory: String,
+      rentAddress: String,
+      rentImageURL: String,
+      rentPrice: Number,
+    };
 
     //4th try
     for (let i = 0; i < requiredFields.length; i++) {
-      const fields = requiredFields[i];
+      // store to looped requiredFields in 'fields'
+      const fieldName = requiredFields[i];
+      //expectedType = looked in to fieldTypes {object} and find the type assigned to this 'fields'
+      const expectedType = fieldTypes[fieldName];
 
-      const value = req.body[fields];
+      const value = req.body[fieldName];
 
       if (value === undefined) {
         return res
           .status(400)
-          .json({ message: `${fields} can't be empty field` });
+          .json({ message: `${fieldName} can't be empty field` });
       }
-      if (typeof value === "string") {
+
+      if (expectedType === String) {
+        if (typeof value !== "string") {
+          return res
+            .status(400)
+            .json({ message: "must input a string ex: abcd.." });
+        }
+
         const trimmedValue = value.trim();
 
         if (trimmedValue === "") {
           return res.status(400).json({
-            message: `${fields} can't have empty string`,
+            message: `${fieldName} can't have empty string`,
           });
         }
-        req.body[fields] = trimmedValue;
+      } else if (expectedType === Number) {
+        if (typeof value !== "number") {
+          return res
+            .status(400)
+            .json({ message: "must input a number ex: 12345.." });
+        }
+        if (isNaN(value)) {
+          return res.status(400).json({ message: "you've input a NaN" });
+        }
       }
     }
 
